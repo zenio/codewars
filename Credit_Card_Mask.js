ad98c686b6221c72deece18399fb8ac16a624264
zenio <zeniocode@gmail.com>
2019-09-05 16:17:48 +0500

    Credit card mask kata solution added

---
+++ Credit_Card_Mask.js
@@ -0,0 +1,20 @@
+/**
+ * Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+ *
+ * Your task is to write a function maskify, which changes all but the last four characters into '#'.
+ * Examples
+ *
+ * maskify("4556364607935616") == "############5616"
+ * maskify(     "64607935616") ==      "#######5616"
+ * maskify(               "1") ==                "1"
+ * maskify(                "") ==                 ""
+ *
+ * // "What was the name of your first pet?"
+ * maskify("Skippy")                                   == "##ippy"
+ * maskify("Nananananananananananananananana Batman!") == "####################################man!"
+ */
+function maskify(string) {
+  const CHARS_TO_MASK = 4;
+  if (string.length <= CHARS_TO_MASK) return string;
+  return "#".repeat(string.length - CHARS_TO_MASK) + string.slice(-CHARS_TO_MASK);
+}
